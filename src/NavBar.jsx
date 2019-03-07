94cf00293a1c91f7d90e2837fcb133d0aef0eb92
Sebastián Restrepo <sebastianrestrepoq@gmail.com>
2019-03-07 17:00:22 -0500

    favicon added

---
+++ src/NavBar.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+const NavBar = function (props) {
+    return <div className="container-fluid">
+        <div className="logo"></div>
+
+    </div>;
+}
+
+export default NavBar;
