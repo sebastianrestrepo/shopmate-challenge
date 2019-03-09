4a6c8db9958a88c76961a7a63b1ddae3109086dd
Sebastián Restrepo <sebastianrestrepoq@gmail.com>
2019-03-09 16:00:53 -0500

    responsive navbar advances

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <NavBar />
+        <header>
+          <NavBar />
+        </header>
       </div>
     );
   }
