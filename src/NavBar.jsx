7c420ceaaaf97846a9cb6123297a0f4881ab2a99
Sebastián Restrepo <sebastianrestrepoq@gmail.com>
2019-03-10 01:32:09 -0500

    hamb icon added

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import Logo from './Logo';
 import Menu from './Menu';
+import HambMenu from './HambMenu';
 import SearchBox from './SearchBox';
+
 
 const NavBar = function (props) {
     return <div className="nav-bar">
         <Logo />
         <Menu />
-
         <div className="nav-btns">
             <SearchBox />
             <button id="bag-btn">
                 <img src="./assets/bag.svg" alt=""></img>
             </ button>
+            <HambMenu />
         </div>
 
     </div>;
