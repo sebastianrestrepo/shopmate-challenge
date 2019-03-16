7380735fc50d6804ce73209d75730a9939d3e192
Sebastián Restrepo <sebastianrestrepoq@gmail.com>
2019-03-15 21:44:34 -0500

    mobx introduced with hamb menu

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,21 +1,10 @@
 import React, { Component } from 'react';
 import NavBar from '../NavBar/NavBar';
-import HambMenu from '../NavBar/HambMenu';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { showHambMenu: false };
-  }
-
-  handleHambMenu() {
-    console.log('show hamb menu');
-    this.setState(prevState => {
-      return { 
-        showHambMenu: !prevState.showHambMenu 
-      };
-    });
   }
 
   render() {
